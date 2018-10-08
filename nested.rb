4ec113c48eb3bdfc169d5845049f8f9c62b76f65
Victor Gonzalez <victorggrodriguez@gmail.com>
2018-10-08 21:44:36 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -122,11 +122,12 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        }
+        },
+				:yukihiro_matsumoto => {
+  				:known_for => "Ruby",
+    			:languages => ["LISP", "C"]
+ }
      }
-
-	programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-	programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 
 end
 
