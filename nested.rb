a203ef0b8403d379b806242c65f29846567e8e1d
Victor Gonzalez <victorggrodriguez@gmail.com>
2018-10-08 21:30:36 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -125,7 +125,7 @@ def adding_to_dennis
         }
      }
 
-		 programmer_hash[:yukihiro_matsumoto]
-
+		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"
 
 end
