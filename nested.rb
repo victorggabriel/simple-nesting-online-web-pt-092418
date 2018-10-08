83538291abfea56ba5e8e01acebf36e3bd01a5e3
Victor Gonzalez <victorggrodriguez@gmail.com>
2018-10-08 21:39:37 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -109,6 +109,25 @@ def changing_alan
 end
 
 def adding_matz
+	programmer_hash =
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+
+	
+
+	
 	programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 	programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 end
