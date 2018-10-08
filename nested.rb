11c324258ab8be974b758c5a0b03043c0c1e38ab
Victor Gonzalez <victorggrodriguez@gmail.com>
2018-10-08 21:26:42 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -125,7 +125,8 @@ def adding_to_dennis
         }
      }
 
-		 programmer_hash[:grace_hopper][:yukihiro_matsumoto]
-		 programmer_hash[:alan_kay][:yukihiro_matsumoto]
- 		 programmer_hash[:dennis_ritchie][:yukihiro_matsumoto]
+		 programmer_hash[:grace_hopper][:adding_matz][:yukihiro_matsumoto]
+		 programmer_hash[:alan_kay][:adding_matz][:yukihiro_matsumoto]
+ 		 programmer_hash[:dennis_ritchie][:adding_matz][:yukihiro_matsumoto]
+
 end
